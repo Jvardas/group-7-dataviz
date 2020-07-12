5a184126c8407105616786ae0c5e553dfc250e78
JVardas <jvardas7@gmail.com>
2020-07-12 22:15:31 +0300

    Updates

---
+++ precache-manifest.b4f184b6f4ba9b2905fcd03e813836b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f54f80ef85713a69e652d32df918d94",
+    "url": "/group-7-dataviz/index.html"
+  },
+  {
+    "revision": "f88ee2689a7caac58d41",
+    "url": "/group-7-dataviz/static/css/main.4add3adc.chunk.css"
+  },
+  {
+    "revision": "df5b4b826aa6587f13d1",
+    "url": "/group-7-dataviz/static/js/2.495f8f25.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/group-7-dataviz/static/js/2.495f8f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88ee2689a7caac58d41",
+    "url": "/group-7-dataviz/static/js/main.5849db04.chunk.js"
+  },
+  {
+    "revision": "6f860a67cff98150985c",
+    "url": "/group-7-dataviz/static/js/runtime-main.7a9344db.js"
+  },
+  {
+    "revision": "d15928677ce9bb98462db752b728974c",
+    "url": "/group-7-dataviz/static/media/JM_ProfilePic.d1592867.jpg"
+  },
+  {
+    "revision": "e92225e7d24ce663ab111aaca3fbad17",
+    "url": "/group-7-dataviz/static/media/JP_ProfilePic.e92225e7.jpg"
+  },
+  {
+    "revision": "d702a63b25770dc4c0a68793223eb917",
+    "url": "/group-7-dataviz/static/media/JV_ProfilePic.d702a63b.jpg"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/group-7-dataviz/static/media/react.37fe8322.png"
+  }
+]);
